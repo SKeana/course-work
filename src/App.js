6c7c1dfb304b12d5142e772bced881f853b7e466
SKeana <Skean2000@outlook.com>
2024-09-23 15:54:32 +0100

    course work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function FoodOrderForm() {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Order Successful!\n\nYour order was ${order}.\n\nPlease show your confirmation number for pickup.`);
+  };
+  
+
+  const [statName, setStatName] = useState('');
+  const [statPhone, setStatPhone]  = useState('')
+  const [stataddress, setStatAddress]  = useState('')
+  const [statOrder, setStatOrder]  = useState('')
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <fourm>
+      <label htmlFor="name">Name:</label>
+      <input id='name' value={statName} onChange={(e) => setName(e.target.value)} name="name"></input>
+      <label htmlFor="phone">name</label>
+      <input id='phone' value={statPhone} onChange={(e) => setStatPhone(e.target.value)} name="phone"></input>
+      <label htmlFor="address">address</label>
+      <input id='address' value={stataddress} onChange={(e) => setStatAddress(e.target.value)} name="address"></input>
+      <label htmlFor="order">order</label>
+      <input id='order' value={statOrder} onChange={(e) => setStatOrder(e.target.value)} name="order"></input>
+      <button type="submit" onSubmit={handleSubmit}>Submit Order</button>
+    </fourm>
+  )
 }
 
-export default App;
+export default FoodOrderForm;
+
